d2269df33d00f7b241c893c59f1455d201d6ac37
Zachary Chao <zach.chao@yahoo.com>
2017-08-29 15:09:13 -0700

    Initial commit for Eclipse Github setup test
    - integrated git with eclipse
    - testing github commit
    - simple hello world program

---
+++ HelloWorld/src/edu/miracosta/cs113/main.java
@@ -0,0 +1,10 @@
+package edu.miracosta.cs113;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Nery");
+
+	}
+
+}
---
+++ HelloWorld/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test Eclipse and Github connection with Hello World
+ */
+/**
+ * @author w7270821
+ *
+ */
+package edu.miracosta.cs113;
